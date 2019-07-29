71ab61aebb350319aec1c5a185cee80204ad9ede
neonnishant <neonnishant123@gmail.com>
2019-07-29 19:02:23 +0530

    added page controller

---
+++ SampleSpringApp/src/main/java/com/example/demo/configuration/ViewResolverConfig.java
@@ -0,0 +1,45 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages= {"com.example.demo"})
+public class ViewResolverConfig implements WebMvcConfigurer {
+
+	@Bean
+	InternalResourceViewResolver viewResolver() {
+		InternalResourceViewResolver invr = new InternalResourceViewResolver();
+		invr.setPrefix("/WEB-INF/pages/");
+		invr.setSuffix(".html");
+		invr.setRequestContextAttribute("requestContext");
+		return invr;
+	}
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		// TODO Auto-generated method stub
+		configurer.enable();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// TODO Auto-generated method stub
+		registry.addResourceHandler("/resources/**").addResourceLocations("/").setCachePeriod(31556926);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		// TODO Auto-generated method stub
+		WebMvcConfigurer.super.addViewControllers(registry);
+	}
+
+}
